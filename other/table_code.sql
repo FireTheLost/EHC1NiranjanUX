80ec355328a6d4aa1ff59aa8e17890061a5506b1
FireTheLost <prajwalmsreddy@gmail.com>
2021-07-22 15:51:21 +0530

    db improvements

--- other/table_code.sql
+++ other/table_code.sql
@@ -2,6 +2,6 @@ CREATE TABLE forum
 (
     forumId int(16) AUTO_INCREMENT PRIMARY KEY NOT NULL,
     forumMessage varchar(256) NOT NULL,
-    forumUser varchar(32) NOT NULL DEFUALT 'Anonymous',
+    forumUser varchar(32) NOT NULL DEFAULT 'Anonymous',
     forumTime varchar(16) NOT NULL
 );
